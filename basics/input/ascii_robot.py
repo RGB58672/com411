533b9c3aa4ae2be0c094c2f11aad36694f4dc8e6
repl.it user <replituser@example.com>
2020-10-01 10:16:00 +0000

    Ascii Robot input

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,8 @@
+# Ask user to enter character for eyes
+print("Please enter a character for the eye")
+character = input()
+print("Beep's expression is now as follows:")
+print("##########")
+print("#  i  i  #")
+print("#  ----  #")
+print("##########")
