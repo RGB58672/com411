459799c39cca889a70feac00ff5a27f7c56b5187
repl.it user <replituser@example.com>
2020-10-01 14:51:07 +0000

    Finally figured out how to do it

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,21 @@
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+# Ask user to enter character for eyes
+print("Please enter a character for the eye")
+eye = input()
+print("Beep's expression is now as follows:")
+print("##########")
+print("#  " + eye + "  " + eye + "  #")
+print("#  ----  #")
+print("##########")
+
+
+1 + 2 
+
+3
+
+"1" + "2"
+"12"
