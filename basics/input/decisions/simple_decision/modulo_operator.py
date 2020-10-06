91e8d8344c391cdbc3960397395c4cfba15945eb
repl.it user <replituser@example.com>
2020-10-06 10:42:02 +0000

    modulo_operator.py

---
+++ basics/input/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,7 @@
+print("Please enter a whole number between 1 and 10")
+number = int(input())
+
+if "(number % 2 == 0)":
+  print ("{} is an even number".format(number))
+else:
+  print ("{} is an odd number" .format(number))
