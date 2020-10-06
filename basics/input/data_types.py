26131a9014d7779017a7c9e181042b580f0efc49
repl.it user <replituser@example.com>
2020-10-06 10:59:10 +0000

    caught up on first lesson

--- basics/input/data_types.py
+++ basics/input/data_types.py
@@ -7,3 +7,22 @@ height = input()
 print("How much do you weigh (in kilograms)?")
 weight = input()
 print(" " + name + " you are"  + age + " years old and your bmi is ") 
+
+# Read in user data 
+print("What is your name?")
+name = input() 
+
+print("What is your age?")
+age = int(input())
+
+print("What is your weight?") 
+weight = float(input()) 
+
+print("What is your height?")
+height = float(input())
+
+# Calculate bmi
+bmi = weight / (height ** 2)
+
+# Display result
+print(name, "your bmi is", bmi
---
+++ basics/input/decisions/simple_decision/comparison_operator.py
@@ -0,0 +1,8 @@
+print("Please enter the first number")
+first_number == int(input())
+print("Please enter the second number")
+second_number == int(input())
+if (first_number) > (second_number):
+  print("The first number is the bigger number")
+else:
+  print("The second number is the bigger number")
