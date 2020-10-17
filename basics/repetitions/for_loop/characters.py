a987d49a702888cb47794d950a715ef85b4b7615
repl.it user <replituser@example.com>
2020-10-17 15:01:49 +0000

    characters.py

---
+++ basics/repetitions/for_loop/characters.py
@@ -0,0 +1,6 @@
+print("What strange markings do you see?")
+markings = input()
+print("Identifying...")
+for count in range (0, len(markings), 1):
+  print("index", count, ":", markings [count])
+print ("Process complete!")
