fb76ebbf4fb840e5051fd1c9203db4aa866f25b7
repl.it user <replituser@example.com>
2020-10-18 13:01:53 +0000

    function_with_loop.py

---
+++ basics/functions/function_with_loop.py
@@ -0,0 +1,8 @@
+def crossed_bridge(steps):
+    for step in range(steps):
+      print("Crossed step.")
+    if (steps > 5):
+        print("The bridge is collapsing!")
+    else:
+        print("We must keep going!")
+crossed_bridge(steps)
