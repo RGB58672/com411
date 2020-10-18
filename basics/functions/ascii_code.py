9e772e8f6ffbcbe42e04b26aa042143efe1e16eb
repl.it user <replituser@example.com>
2020-10-18 11:12:01 +0000

    ascii_character.py

--- basics/functions/ascii_code.py
+++ basics/functions/ascii_code.py
@@ -2,7 +2,7 @@ print("Program Started!")
 print("Please enter a standard character:")
 character = input()
 if(len(character) == 1):
-  print("The ASCII code for {} is {}."format(character, ord(character))
+  print("The ASCII code for {} is {}".format(character, ord(character))
 else:
   print("Please enter a single character.")
 print("Program Ended!")
