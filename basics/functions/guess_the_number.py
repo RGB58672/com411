ef396092ba2108483bf2af758deb28b207427de4
repl.it user <replituser@example.com>
2020-10-20 09:12:41 +0000

    simple_list.py

---
+++ basics/functions/guess_the_number.py
@@ -0,0 +1,6 @@
+import random
+print("Please enter the minimum value:")
+minimum = int(input())
+print("Please eneter the maximum value:")
+maximum = int(input())
+random_number = random.randrange(min_value, max_value)
---
+++ data/lists/simple_list.py
@@ -0,0 +1,6 @@
+def directions():
+  directions = ["Move Forward, Move Backwards, Move Left and Move Right"]
+  return directions
+def run():
+  print(directions())
+run()
